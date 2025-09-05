ac68cd58a49d1d458e18e48b9835acd2b5b326cb
Ndzuma Malate <k2253653@kingston.ac.uk>
2025-09-05 22:01:46 +0100

    Remove custom webpack cache configuration

--- next.config.mjs
+++ next.config.mjs
@@ -9,18 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    // Fix for large string serialization issue
-    config.cache = {
-      ...config.cache,
-      cacheDirectory: config.cache.cacheDirectory,
-      type: "filesystem",
-      buildDependencies: config.cache.buildDependencies,
-      cacheCompression: false,
-      maxMemoryGenerations: 1,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
