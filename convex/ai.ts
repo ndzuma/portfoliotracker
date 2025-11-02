1bb9f1670b10228d4b2d028ef2a3fbb494b12c0f
Ndzuma Malate <k2253653@kingston.ac.uk>
2025-11-02 14:27:57 +0000

    Update ai.ts

--- convex/ai.ts
+++ convex/ai.ts
@@ -71,3 +71,44 @@ export const getAiNewsSummary = query({
     return latestSummary || null;
   },
 });
+
+export const generateAiPortfolioSummary = action({
+  handler: async (ctx, args) => {
+    // test the urls
+    if (!marketDataServiceUrl) {
+      throw new Error("MARKET_DATA_SERVICE_URL is not defined");
+    }
+    if (!aiServiceUrl) {
+      throw new Error("AI_SERVICE_URL is not defined");
+    }
+    
+    // get news articles from last 24 hours
+    const newsArticlesResponse = await fetch(`${marketDataServiceUrl}/news`)
+    if (!newsArticlesResponse.ok) {
+      throw new Error(`Failed to fetch market news: ${newsArticlesResponse.statusText}`);
+    }
+    const newsArticles = await newsArticlesResponse.json();
+    
+    // call external ai endpoint to generate summary
+    const aiSummaryResponse = await fetch(`${aiServiceUrl}/api/v1/portfolio`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ news_data: newsArticles.Data }),
+    });
+    if (!aiSummaryResponse.ok) {
+      throw new Error(`Failed to generate AI summary: ${aiSummaryResponse.statusText}`);
+    }
+    const aiSummary = await aiSummaryResponse.json();
+    
+    // store summary in convex database
+    await ctx.runMutation(internal.ai.storeAiNewsSummary, {
+      analysis: aiSummary.analysis,
+      timestamp: new Date(aiSummary.timestamp).getTime(), 
+      modelUsed: aiSummary.model_used,
+      tokensUsed: aiSummary.tokens_used,
+      processingTimeMs: aiSummary.processing_time_ms,
+    });
+  },
+});
