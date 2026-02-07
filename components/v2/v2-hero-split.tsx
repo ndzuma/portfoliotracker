d5decdd16654267d019f120ba703e59c4d57d69f
Ndzuma Malate <k2253653@kingston.ac.uk>
2026-02-07 01:06:21 +0000

    Reduce hero vertical padding and side padding

--- components/v2/v2-hero-split.tsx
+++ components/v2/v2-hero-split.tsx
@@ -10,16 +10,16 @@ interface V2HeroSplitProps {
 export function V2HeroSplit({ leftContent, rightContent }: V2HeroSplitProps) {
   return (
     <section className="border-b" style={{ borderColor: "rgba(255,255,255,0.06)" }}>
-      <div className="max-w-[1600px] mx-auto px-8 py-10">
+      <div className="max-w-[1600px] mx-auto px-8 py-8">
         <div className="flex flex-col lg:flex-row">
           {/* Left - 60% */}
-          <div className="flex-[0.6] pr-0 lg:pr-10">{leftContent}</div>
+          <div className="flex-[0.6] pr-0 lg:pr-6">{leftContent}</div>
 
           {/* Vertical Divider */}
           <div className="hidden lg:block w-px bg-white/[0.06] mx-0 shrink-0" />
 
           {/* Right - 40% */}
-          <div className="flex-[0.4] pl-0 lg:pl-10 mt-8 lg:mt-0">{rightContent}</div>
+          <div className="flex-[0.4] pl-0 lg:pl-6 mt-8 lg:mt-0">{rightContent}</div>
         </div>
       </div>
     </section>
